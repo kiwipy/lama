e59f9416c5cbab20fc4bd5d4aab188f6968fa3a5
William Andersson <william.andersson@outlook.com>
2024-10-03 17:25:20 +0200

    lama.py
    Version 0.3.0
    No release notes (experimental)

--- lama.py
+++ lama.py
@@ -6,7 +6,7 @@
 # Website:     https://github.com/william-andersson
 # License:     GPL
 #
-VERSION="0.2.1"
+VERSION="0.3.0"
 import ollama
 import os.path
 import os
@@ -15,6 +15,7 @@ import json
 import readline
 
 #################### Don't change ####################
+StringLen = 0
 VERBOSE = False
 OPTIONS = {'num_ctx': 4096}
 HISTORY = []
@@ -28,43 +29,49 @@ ANTONYM = {'I': 'you', 'me': 'you',
            'yourself': 'myself', 'myself': 'yourself'}
 ######################################################
 
-if len(sys.argv) < 3:
-    print("Usage: lama.py <model> <conversation_name>")
-    sys.exit(1)
-else:
-    MODEL = sys.argv[1]
-    HOME = os.path.expanduser( '~/' )
-    if not os.path.exists("%s.lama" % HOME):
-        os.makedirs("%s.lama" % HOME)
-    SOURCE = HOME + '.lama/' + sys.argv[2]
-if sys.argv[-1] == "-v":
-    VERBOSE = True
-    print(f"lama.py: Running in verbose\n" \
-          f"lama.py: Using model: {MODEL}\n" \
-          f"lama.py: Session: {SOURCE}")
-
 def loadDB():
     global HISTORY, SOURCE
     if os.path.exists('%s' % SOURCE):
         with open('%s' % SOURCE, 'r') as db:
             HISTORY = json.load(db)
-            if VERBOSE == True:
-                print("lama.py: Session loaded")
     else:
-        if VERBOSE == True:
-            print("lama.py: Creating new session")
         saveDB()
 
 def saveDB():
     global HISTORY, SOURCE
     with open('%s' % SOURCE, 'w') as db:
         json.dump(HISTORY, db)
-        if VERBOSE == True:
-            print("lama.py: Session saved")
+
+def format(feed, TermSize):
+    # Implement some crude wordwrapping using StringLen, TermSize and size of
+    # current and next item.
+    global StringLen
+    if len(feed) > 1 and len(feed[-1]) != 0:
+        if feed[-1][0] != ' ' and \
+                StringLen + len(feed[-2]) + len(feed[-1]) > \
+                TermSize.columns - 2 and '\n' not in feed[-2]:
+            print(f"\n{feed[-2].lstrip()}", end='', flush=True)
+            StringLen = len(feed[-2]) - 1
+        elif (StringLen + len(feed[-2])) > TermSize.columns - 2:
+            print(f"\n{feed[-2].lstrip()}", end='', flush=True)
+            StringLen = len(feed[-2]) - 1
+        else:
+            print(f"{feed[-2]}", end='', flush=True)
+            if '\n' in feed[-2]:
+                StringLen = 0
+            else:
+                StringLen += len(feed[-2])
+    if len(feed[-1]) == 0:
+        if (StringLen + len(feed[-2]) + len(feed[-1]) + 3) > TermSize.columns - 1:
+            print(f"\n{feed[-2].lstrip()}{feed[-1]}", end='', flush=True)
+        else:
+            print(f"{feed[-2]}{feed[-1]}", end='', flush=True)
+        StringLen = 0
 
 def run_query():
     global MODEL, OPTIONS, HISTORY, OPTIMIZE, ANTONYM
     answer = ''
+    feed = []
     
     stream = ollama.chat(
         model = MODEL,
@@ -73,22 +80,15 @@ def run_query():
         options = OPTIONS
     )
 
-    StringLen = 0
     TermSize = os.get_terminal_size()
     for chunk in stream:
         # Collect the output from assistant.
         answer += chunk['message']['content']
+        feed.append(chunk['message']['content'])
         if OPTIMIZE != True:
             # Don't print output from optimize function.
-            # Implement some crude wordwrapping using StringLen.
-            StringLen += len(chunk['message']['content'])
-            if (StringLen + 4) > TermSize.columns:
-                print(f"\n{chunk['message']['content']}", end='', flush=True)
-                StringLen = len(chunk['message']['content'])
-            else:
-                print(f"{chunk['message']['content']}", end='', flush=True)
-                if '\n' in chunk['message']['content']:
-                    StringLen = 0
+            format(feed, TermSize)
+
     if OPTIMIZE == True:
         return answer
     else:
@@ -99,7 +99,7 @@ def run_query():
 
 def optimize():
     # If history exceeds 95% of num_ctx, compress history stack.
-    global HISTORY, OPTIMIZE, OPTIONS
+    global HISTORY, OPTIMIZE, OPTIONS, ANTONYM
     limit = int(OPTIONS['num_ctx']*0.95)
     h_size = 0
     
@@ -107,14 +107,11 @@ def optimize():
         h_size += len(i['content'].split())
     h_size = h_size*2
     if VERBOSE == True:
-        print(f"lama.py: Token count [{h_size}]")
+        print(f"\nlama.py: Token count {h_size}\n")
 
     if h_size >= limit:
         OPTIMIZE = True
-        if VERBOSE == True:
-            print("lama.py: optimizing history stack")
-        else:
-            print("\n[Optimizing history stack...]")
+        print("\n[Optimizing history stack...]")
         CmdPrompt = {'role': 'user', 'content': 'compress conversation history'}
         HISTORY.append(CmdPrompt)
         answer = run_query()
@@ -135,6 +132,21 @@ def optimize():
                 answer_mod += ' '
         text_output['content'] = answer_mod
         HISTORY.append(text_output)
+
+if len(sys.argv) < 3:
+    print("Usage: lama.py <model> <conversation_name>")
+    sys.exit(1)
+else:
+    MODEL = sys.argv[1]
+    HOME = os.path.expanduser( '~/' )
+    if not os.path.exists("%s.lama" % HOME):
+        os.makedirs("%s.lama" % HOME)
+    SOURCE = HOME + '.lama/' + sys.argv[2]
+if sys.argv[-1] == "-v":
+    VERBOSE = True
+    print(f"lama.py: Running in verbose")
+    print(f"lama.py: Using model: {MODEL}")
+    print(f"lama.py: Session: {SOURCE}")
 
 loadDB()
 while True:
